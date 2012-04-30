46aa0654d35438f580948e573bb17823f24fc00d
EKR <ekr@rtfm.com>
2012-04-30 11:09:57 -0700

    Working socket transport unit test
    --HG--
    branch : ekr_transport_branch

--- media/mtransport/test/sockettransportservice_unittest.cpp
+++ media/mtransport/test/sockettransportservice_unittest.cpp
@@ -5,6 +5,8 @@
 // Original author: ekr@rtfm.com
 #include <iostream>
 
+#include "prio.h"
+
 #include "nsCOMPtr.h"
 #include "nsNetCID.h"
 #include "nsXPCOMGlue.h"
@@ -14,7 +16,9 @@
 #include "nsIComponentRegistrar.h"
 #include "nsIIOService.h"
 #include "nsIServiceManager.h"
+#include "nsISocketTransportService.h"
 
+#include "nsASocketHandler.h"
 #include "nsServiceManagerUtils.h"
 #include "nsThreadUtils.h"
 
@@ -22,24 +26,33 @@
 #include "gtest/gtest.h"
 #include "gtest_utils.h"
 
-class PacketReceived;
 
 namespace {
 class SocketTransportServiceTest : public ::testing::Test {
  public:
-  SocketTransportServiceTest() : received_(0) {
+  SocketTransportServiceTest() : received_(0),
+                                 readpipe_(NULL),
+                                 writepipe_(NULL),
+                                 registered_(NULL) {
   }
 
-  void SetUp() {
-    nsresult rv;
-
-    target_ = do_GetService(NS_SOCKETTRANSPORTSERVICE_CONTRACTID, &rv);
-    ASSERT_TRUE(NS_SUCCEEDED(rv));
+  ~SocketTransportServiceTest() {
+    if (readpipe_)
+      PR_Close(readpipe_);
+    if (writepipe_)
+      PR_Close(writepipe_);
   }
 
-  void Run();
+  void SetUp();
+  void RegisterHandler();
+  void SendEvent();
+  void SendPacket();
 
   void ReceivePacket() {
+    ++received_;
+  }
+
+  void ReceiveEvent() {
     ++received_;
   }
 
@@ -48,35 +61,131 @@ class SocketTransportServiceTest : public ::testing::Test {
   }
 
  private:
+  nsCOMPtr<nsISocketTransportService> stservice_;
   nsCOMPtr<nsIEventTarget> target_;
   size_t received_;
+  PRFileDesc *readpipe_;
+  PRFileDesc *writepipe_;
+  bool registered_;
 };
 
 
-class PacketReceived : public nsRunnable {
+// Received an event.
+class EventReceived : public nsRunnable {
 public:
-  PacketReceived(SocketTransportServiceTest *test) :
+  EventReceived(SocketTransportServiceTest *test) :
       test_(test) {};
     
   NS_IMETHOD Run() {
-    test_->ReceivePacket();
+    test_->ReceiveEvent();
     return NS_OK;
   }
 
   SocketTransportServiceTest *test_;
 };
 
-TEST_F(SocketTransportServiceTest, Test) {
-  Run();
+
+// Register our listener on the socket
+class RegisterEvent : public nsRunnable {
+public:
+  RegisterEvent(SocketTransportServiceTest *test) :
+      test_(test) {};
+    
+  NS_IMETHOD Run() {
+    test_->RegisterHandler();
+    return NS_OK;
+  }
+
+  SocketTransportServiceTest *test_;
+};
+
+
+class SocketHandler : public nsASocketHandler {
+ public:
+  SocketHandler(SocketTransportServiceTest *test) : test_(test) {
+  }
+
+  void OnSocketReady(PRFileDesc *fd, PRInt16 outflags) {
+    unsigned char buf[1600];
+    
+    PRInt32 rv;
+    rv = PR_Recv(fd, buf, sizeof(buf), 0, PR_INTERVAL_NO_WAIT);
+    if (rv > 0) {
+      std::cerr << "Read " << rv << " bytes" << std::endl;
+      test_->ReceivePacket();
+    }
+  }
+
+  void OnSocketDetached(PRFileDesc *fd) {}
+
+  NS_DECL_ISUPPORTS
+  
+ private:
+  SocketTransportServiceTest *test_;
+};
+
+NS_IMPL_ISUPPORTS0(SocketHandler);
+
+void SocketTransportServiceTest::SetUp() {
+  // Get the transport service as a dispatch target
+  nsresult rv;
+  target_ = do_GetService(NS_SOCKETTRANSPORTSERVICE_CONTRACTID, &rv);
+  ASSERT_TRUE(NS_SUCCEEDED(rv));
+
+  // Get the transport service as a transport service
+  stservice_ = do_GetService(NS_SOCKETTRANSPORTSERVICE_CONTRACTID, &rv);
+  ASSERT_TRUE(NS_SUCCEEDED(rv));
+
+  // Create a loopback pipe
+  PRStatus status = PR_CreatePipe(&readpipe_, &writepipe_);
+  ASSERT_EQ(status, PR_SUCCESS);
+
+  // Register ourselves as a listener for the read side of the
+  // socket. The registration has to happen on the STS thread,
+  // hence this event stuff.
+  rv = target_->Dispatch(new RegisterEvent(this), 0);
+  ASSERT_TRUE(NS_SUCCEEDED(rv));
+  ASSERT_TRUE_WAIT(registered_, 10000);
+
 }
 
-void SocketTransportServiceTest::Run() {
+void SocketTransportServiceTest::RegisterHandler() {
   nsresult rv;
 
-  target_->Dispatch(new PacketReceived(this), rv);
-  ASSERT_TRUE(rv);
+  rv = stservice_->AttachSocket(readpipe_, new SocketHandler(this));
+  ASSERT_TRUE(NS_SUCCEEDED(rv));  
+  
+  registered_ = true;
+}
+
+void SocketTransportServiceTest::SendEvent() {
+  nsresult rv;
+
+  rv = target_->Dispatch(new EventReceived(this), 0);
+  ASSERT_TRUE(NS_SUCCEEDED(rv));
   ASSERT_TRUE_WAIT(Received() == 1, 10000);
 }
+
+void SocketTransportServiceTest::SendPacket() {
+  unsigned char buffer[1024];
+  memset(buffer, 0, sizeof(buffer));
+  
+  PRInt32 status = PR_Write(writepipe_, buffer, sizeof(buffer));
+  PRUint32 size = status & 0xffff;
+  ASSERT_EQ(sizeof(buffer), size);
+}
+
+
+
+// The unit tests themselves
+TEST_F(SocketTransportServiceTest, SendEvent) {
+  SendEvent();
+}
+
+TEST_F(SocketTransportServiceTest, SendPacket) {
+  SendPacket();
+}
+
 
 }  // end namespace
 
