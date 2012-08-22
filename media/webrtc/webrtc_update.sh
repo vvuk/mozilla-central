093aaa227be1e4dbab90c8034abfaabc1fdb3f25
Randell Jesup <rjesup@jesup.org>
2012-08-22 05:02:40 -0400

    Bug 688178: Update webrtc_update.sh to turn on merges of pending upstream patches
    --HG--
    extra : rebase_source : 3a7c0f5a54e76c78767bf00b5f6abb5feec7737f

--- media/webrtc/webrtc_update.sh
+++ media/webrtc/webrtc_update.sh
@@ -1,41 +1,7 @@
 #!/bin/bash
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is Mozilla code.
-#
-# The Initial Developer of the Original Code is the Mozilla Foundation.
-# Portions created by the Initial Developer are Copyright (C) 2010
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#  Randell Jesup <rjesup@jesup.org>
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK *****
-
-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # First, get a new copy of the tree to play with
 # They both want to be named 'trunk'...
@@ -94,15 +60,15 @@ echo "You probably want to do these from another shell so you can look at these"
 hg commit -m "Webrtc import $revision"
 # webrtc-import-last is auto-updated (bookmark)
 
-#echo ""
-#echo "hg update --clean webrtc-pending"
-#echo "hg merge -r webrtc-import-last"
-#echo "hg commit -m 'merge latest import to pending, rev blah'"
+echo ""
+hg update --clean webrtc-pending
+hg merge -r webrtc-import-last
+hg commit -m 'merge latest import to pending, $revision'
 # webrtc-pending is auto-updated (bookmark)
 
 echo ""
 hg update --clean webrtc-trim
-hg merge -r webrtc-import-last
+hg merge -r webrtc-pending
 hg commit -m "merge latest import to trim, $revision"
 # webrtc-trim is auto-updated (bookmark)
 
