5527c2409118db532ab17db94d6f992b732472d4
Ted Mielczarek <ted.mielczarek@gmail.com>
2012-08-31 19:06:23 -0400

    bug 787176 - bustage fix, with statement

--- testing/runcppunittests.py
+++ testing/runcppunittests.py
@@ -4,6 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from __future__ import with_statement
 import sys, optparse, os, tempfile, shutil
 import mozprocess, mozinfo, mozlog, mozcrash
 from contextlib import contextmanager
